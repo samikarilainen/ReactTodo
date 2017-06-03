cacfe77cf91d268285a1b075e14d29ce0d89c843
Sami Karilainen <sami@Samis-Air.home>
2017-06-03 11:57:54 +0300

    Added lists

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Make TODO app'
+        },
+        {
+          id: 4,
+          text: 'Play Diablo'
+        }
+      ]
+    };
+  },
   render: function () {
+    var {todos} = this.state;
     return (
       <div>
-        Todo App.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
