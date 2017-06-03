a9df934f8562cfe93b591256411f23e616568e50
Sami Karilainen <sami@Samis-Air.home>
2017-06-03 13:55:51 +0300

    Added tests

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,9 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require ('jQuery');
+
 var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
